14c76495f2473f8a9daf6113f0d8b0590d2de63f
Tatiwat Borwornrattanakit <TL3@Tatiwat-MacBook-Pro.local>
2016-05-02 20:06:08 +0700

    init tableview

--- MusicVideo/ViewController.swift
+++ MusicVideo/ViewController.swift
@@ -8,10 +8,11 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
     
     var videos = [Videos]()
 
+    @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var displayLabel: UILabel!
     
     override func viewDidLoad() {
@@ -41,6 +42,8 @@ class ViewController: UIViewController {
             print("name = \(item.vName)")
         }
         
+        tableView.reloadData()
+        
     }
     
     func reachabilityStatusChanges() {
@@ -61,6 +64,26 @@ class ViewController: UIViewController {
     // Is call just as the object is about to be deallocated
     deinit {
         NSNotificationCenter.defaultCenter().removeObserver(self, name: "ReachStatusChanged", object: nil)
+    }
+    
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return videos.count
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCellWithIdentifier("cell", forIndexPath: indexPath)
+        
+        let video = videos[indexPath.row]
+        
+        cell.textLabel?.text = ("\(indexPath.row + 1)")
+        
+        cell.detailTextLabel?.text = video.vName
+        
+        return cell
     }
     
 }
