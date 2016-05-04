8c31dc51fcb45f64103fc67f6e90c4cd6f5c7fe8
Tatiwat Borwornrattanakit <TL3@Tatiwat-MacBook-Pro.local>
2016-05-04 23:53:11 +0700

    load image and cache

--- MusicVideo/MusicVideoTableViewCell.swift
+++ MusicVideo/MusicVideoTableViewCell.swift
@@ -24,8 +24,44 @@ class MusicVideoTableViewCell: UITableViewCell {
         
         musicTitle.text = video?.vName
         rank.text = "\(video!.vRank)"
-        musicImage.image = UIImage(named: "imageNotAvailable")
+        //musicImage.image = UIImage(named: "imageNotAvailable")
         
+        if video!.vImageData != nil {
+            print("Get data from array ...")
+            musicImage.image = UIImage(data: video!.vImageData!)
+        }
+        else {
+            GetVideoImage(video!, imageView: musicImage)
+            print("Get images in background thread")
+        }
+        
+    }
+    
+    func GetVideoImage(video: Videos, imageView : UIImageView){
+        
+        // Background thread
+        //  DISPATCH_QUEUE_PRIORITY_HIGH Items dispatched to the queue will run at high priority, i.e. the queue will be scheduled for execution before any default priority or low priority queue.
+        //
+        //  DISPATCH_QUEUE_PRIORITY_DEFAULT Items dispatched to the queue will run at the default priority, i.e. the queue will be scheduled for execution after all high priority queues have been scheduled, but before any low priority queues have been scheduled.
+        //
+        //  DISPATCH_QUEUE_PRIORITY_LOW Items dispatched to the queue will run at low priority, i.e. the queue will be scheduled for execution after all default priority and high priority queues have been scheduled.
+        
+        
+        dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0)) {
+            
+            let data = NSData(contentsOfURL: NSURL(string: video.vImageUrl)!)
+            
+            var image : UIImage?
+            if data != nil {
+                video.vImageData = data
+                image = UIImage(data: data!)
+            }
+            
+            // move back to Main Queue
+            dispatch_async(dispatch_get_main_queue()) {
+                imageView.image = image
+            }
+        }
     }
 
 }
