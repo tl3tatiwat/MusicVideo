8074ac0e17fa27196c2c692da5a813aa1edddae0
Tatiwat Borwornrattanakit <TL3@Tatiwat-MacBook-Pro.local>
2016-05-05 21:04:40 +0700

    share to other application

--- MusicVideo/MusicVideoDetailVC.swift
+++ MusicVideo/MusicVideoDetailVC.swift
@@ -39,6 +39,50 @@ class MusicVideoDetailVC: UIViewController {
     
     @IBAction func socialMedia(sender: UIBarButtonItem) {
         
+        shareMedia()
+        
+    }
+    
+    func shareMedia() {
+        
+        let activity1 = "Have you had the opportunity to see this Music Video?"
+        let activity2 = ("\(videos.vName) by \(videos.vArtist)")
+        let activity3 = "Watch it and tell me what you think?"
+        let activity4 = videos.vLinkToiTunes
+        let activity5 = "(Shared with the Music Video App)"
+        
+        let activityViewController : UIActivityViewController = UIActivityViewController(activityItems: [activity1, activity2, activity3, activity4,activity5], applicationActivities: nil)
+        
+        //activityViewController.excludedActivityTypes =  [UIActivityTypeMail]
+        
+        
+        
+        //        activityViewController.excludedActivityTypes =  [
+        //            UIActivityTypePostToTwitter,
+        //            UIActivityTypePostToFacebook,
+        //            UIActivityTypePostToWeibo,
+        //            UIActivityTypeMessage,
+        //            UIActivityTypeMail,
+        //            UIActivityTypePrint,
+        //            UIActivityTypeCopyToPasteboard,
+        //            UIActivityTypeAssignToContact,
+        //            UIActivityTypeSaveToCameraRoll,
+        //            UIActivityTypeAddToReadingList,
+        //            UIActivityTypePostToFlickr,
+        //            UIActivityTypePostToVimeo,
+        //            UIActivityTypePostToTencentWeibo
+        //        ]
+        
+        activityViewController.completionWithItemsHandler = {
+            (activity, success, items, error) in
+            
+            if activity == UIActivityTypeMail {
+                print ("email selected")
+            }
+            
+        }
+        
+        self.presentViewController(activityViewController, animated: true, completion: nil)
     }
 
     @IBAction func platVideo(sender: UIBarButtonItem) {
